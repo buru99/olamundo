4c2f25d20d44a2dc9ba91cb17da305aea8a3a2e3
buru99 <155699998+buru99@users.noreply.github.com>
2024-06-13 02:36:33 -0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,6 +1,17 @@
 import flet as ft
-#este é o meu site flet
+import webbrowser
+
 def main(page: ft.Page):
-    page.add(ft.Text("Hello World"))
+    
+    def botao(e):
+        link = "https://www.amazon.com.br/?&tag=hydrbrabk-20&ref=pd_sl_7rwd1q78df_e&adgrpid=155790195778&hvpone=&hvptwo=&hvadid=677606588104&hvpos=&hvnetw=g&hvrand=8968211671279102359&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001540&hvtargid=kwd-10573980&hydadcr=26346_11691057&gad_source=1"
+        webbrowser.open(link)
+    
+    button = ft.TextButton("link", on_click=botao)
+    
+    
+    
+    page.add(button)
+    
 
 ft.app(main)
