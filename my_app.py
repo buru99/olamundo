59a8cc48c79e16029432e45a21884f6cec319ae5
buru99 <155699998+buru99@users.noreply.github.com>
2024-06-15 00:54:13 -0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -5,7 +5,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title="meu app flet"
     def link(e):
-        page.launch_url("https://www.youtube.com/")
+        page.launch_url("https://validadedigital.pages.dev/")
     botao = ft.TextButton("link", on_click=link)
     page.add(ft.Text("Meu aplicativo"), botao)
 ft.app(main)
